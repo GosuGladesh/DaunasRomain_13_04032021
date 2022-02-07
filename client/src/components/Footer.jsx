0d37358bd01e3c9dbcd0b628564cd23e584b37d7
Romain DAUNAS <romain.daunas@gmail.com>
2022-02-07 14:32:53 +0100

    Front end

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(props) {
+    return (
+        <footer class="footer">
+      <p class="footer-text">Copyright 2020 Argent Bank</p>
+    </footer>
+    )
+}
+
+export default Footer;
